86b41ff5d45956374c9bff9b610461e72a5eac52
Eredah <jprocetti@gmail.com>
2022-03-15 22:44:19 -0300

    Added a Palette feature;

--- pixel-sketcher.js
+++ pixel-sketcher.js
@@ -1,3 +1,4 @@
+//EVENTS
 //Update Canva Size
 const canva = document.getElementById("canva");
 const size_slider = document.getElementById("size-slider");
@@ -71,6 +72,18 @@ document.addEventListener("mouseup", function () {
     is_mouse_pressed = false;
 })
 
+//get color from palette
+const palette = document.getElementById("palette");
+palette.addEventListener("click", function (event) {
+    let color = document.getElementById("color");
+    let target = event.target;
+    if (target.style.backgroundColor && target.style.backgroundColor != "transparent") {
+        color.value = rgb2hex(target.style.backgroundColor)
+        color_wrapper.style.backgroundColor = color_menu.value;
+    }
+})
+
+//FUNCTIONS
 //Create canva grid
 function create_canva(size) {
     size = typeof size === typeof 0? size : console.log(`ERROR! Invalid canva size. Type:${typeof size} Value:${size}`);
@@ -95,6 +108,7 @@ function create_canva(size) {
         new_div.setAttributeNode(att);
         canva.appendChild(new_div);
     }
+    create_palette();
 }
 //Based on selected tool use it on target cell
 function use_tool(trgt) {
@@ -122,6 +136,59 @@ function use_tool(trgt) {
     
         default:
             break;
+    }
+    create_palette();
+}
+//read canva divs and return an array of all colors used on it
+function create_palette() {
+    let cells = canva.children;
+    let palette = [];
+    for (let i = 0; i < cells.length; i++) {
+        const element = cells[i];
+        const canva_color = element.style.backgroundColor;
+        if (!canva_color || canva_color == "transparent") {
+            continue;
+        }
+        if (!palette.includes(canva_color)) {
+            palette.push(canva_color);
+        }
+    }
+    show_palette(palette);
+}
+//display palette on screen
+function show_palette(pal) {
+    const palette_box = document.getElementById("palette");
+    //delete all content on palette
+    while (palette_box.firstChild) {
+        palette_box.removeChild(palette_box.lastChild);
+    }
+    //set the grid
+    //used the comments to reach an responsive grid equation so i'll leave it here since this is a exercise project:
+    //min columns = 3; rows = columns * 3
+    //c? e r? e cellsize? area=w*h; ratio = h/w; 
+    // r/c = 3 > r = 3c;
+    // area = r*c > area = 3*csquare > c = sq area/3
+    let ratio = palette_box.offsetHeight/palette_box.offsetWidth
+    let cols = Math.max(Math.ceil(Math.sqrt((pal.length/ratio))), 3);
+    let rows = cols*ratio
+    palette_box.style.setProperty("grid-template-columns", `repeat(${cols},1fr)`)
+    palette_box.style.setProperty("grid-template-rows", `repeat(${rows},1fr)`)
+
+    //add area amount of divs on palette_box
+    for (let i = 0; i < pal.length; i++) {
+        let new_div = document.createElement("div");
+        let att = document.createAttribute("class");
+        att.value = `palette-cell`;
+        new_div.setAttributeNode(att);
+        att = document.createAttribute("id");
+        att.value = `cell-${i}`;
+        new_div.setAttributeNode(att);
+        att = document.createAttribute("onmousedown");
+        att.value = "return false";
+        new_div.setAttributeNode(att);
+        new_div.style.backgroundColor = pal[i]
+        new_div.style.border = "0.1px solid white";
+        palette_box.appendChild(new_div);
     }
 }
 //convert rgb sintax to hex
